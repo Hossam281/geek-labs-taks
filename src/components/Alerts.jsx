8b3e96c91c588276821470ccc90960b98be61148
Hossam281 <mohamedhosam281@outlook.com>
2024-05-15 15:50:01 +0300

    fixing search

--- src/components/Alerts.jsx
+++ src/components/Alerts.jsx
@@ -1,99 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Notify from "../assets/notify.svg?react";
 import Search from "../assets/search.svg?react";
 import AlertItem from "./AlertItem";
 import Filters from "./Filters";
 import Close from "../assets/close.svg?react";
-import {
-  Dialog,
-  DialogPanel,
-  Transition,
-  TransitionChild,
-} from "@headlessui/react";
+import { Dialog, DialogPanel, Transition, TransitionChild } from "@headlessui/react";
 
 const Alerts = () => {
   const [search, setSearch] = useState("");
   const [data, setData] = useState([
-    {
-      id: 1,
-      name: "AMZN",
-      price: 200,
-      profit: 0.25,
-      risk: "Low",
-    },
-    {
-      id: 2,
-      name: "AAPL",
-      price: 150,
-      profit: -0.1,
-      risk: "High",
-    },
-    {
-      id: 3,
-      name: "GOOGL",
-      price: 300,
-      profit: 0.15,
-      risk: "Low",
-    },
-    {
-      id: 4,
-      name: "MSFT",
-      price: 180,
-      profit: -0.05,
-      risk: "High",
-    },
-    {
-      id: 5,
-      name: "TSLA",
-      price: 700,
-      profit: 0.3,
-      risk: "High",
-    },
-    {
-      id: 6,
-      name: "NFLX",
-      price: 400,
-      profit: -0.2,
-      risk: "Low",
-    },
-    {
-      id: 7,
-      name: "NVDA",
-      price: 150,
-      profit: 0.1,
-      risk: "Low",
-    },
-    {
-      id: 8,
-      name: "FB",
-      price: 320,
-      profit: -0.15,
-      risk: "High",
-    },
-    {
-      id: 9,
-      name: "BABA",
-      price: 220,
-      profit: 0.05,
-      risk: "Low",
-    },
-    {
-      id: 10,
-      name: "AMD",
-      price: 120,
-      profit: -0.08,
-      risk: "High",
-    },
+    { id: 1, name: "AMZN", price: 200, profit: 0.25, risk: "Low" },
+    { id: 2, name: "AAPL", price: 150, profit: -0.1, risk: "High" },
+    { id: 3, name: "GOOGL", price: 300, profit: 0.15, risk: "Low" },
+    { id: 4, name: "MSFT", price: 180, profit: -0.05, risk: "High" },
+    { id: 5, name: "TSLA", price: 700, profit: 0.3, risk: "High" },
+    { id: 6, name: "NFLX", price: 400, profit: -0.2, risk: "Low" },
+    { id: 7, name: "NVDA", price: 150, profit: 0.1, risk: "Low" },
+    { id: 8, name: "FB", price: 320, profit: -0.15, risk: "High" },
+    { id: 9, name: "BABA", price: 220, profit: 0.05, risk: "Low" },
+    { id: 10, name: "AMD", price: 120, profit: -0.08, risk: "High" },
   ]);
   const [filteredData, setFilteredData] = useState(data);
   const [isOpen, setIsOpen] = useState(false);
   const [filterOptions, setFilterOptions] = useState(null);
 
   const applyFilters = () => {
-    if (filterOptions.risk) {
-      const newData = data.filter((item) =>
-        item.risk.includes(filterOptions.risk)
-      );
+    if (filterOptions && filterOptions.risk) {
+      const newData = data.filter((item) => item.risk === filterOptions.risk);
       setFilteredData(newData);
     } else {
       setFilteredData(data);
@@ -106,13 +39,12 @@ const Alerts = () => {
   };
 
   return (
-    <main className="flex justify-between  w-full gap-3 h-full">
+    <main className="flex justify-between w-full gap-3 h-full">
       <div className="flex flex-col w-full gap-3 h-full">
         <header className="flex gap-3 mb-2 items-center">
-          <h2 className="text-sm  md:text-4xl font-bold border-l-2 md:border-l-5 border-[#53ACFF] px-4">
+          <h2 className="text-sm md:text-4xl font-bold border-l-2 md:border-l-5 border-[#53ACFF] px-4">
             Alerts
           </h2>
-
           <div className="relative w-[70%] md:w-[50%]">
             <input
               type="text"
@@ -125,26 +57,25 @@ const Alerts = () => {
           </div>
           <Notify className="w-7 h-7 hidden md:block" />
           <button
-            className=" block md:hidden ml-auto px-4 rounded-md  text-xs bg-[#53ACFF] p-2"
+            className="block md:hidden ml-auto px-4 rounded-md text-xs bg-[#53ACFF] p-2"
             onClick={() => setIsOpen(true)}
           >
             Filter
           </button>
         </header>
-        <section className="flex  flex-col gap-3 h-full w-full  overflow-y-scroll">
-          {filteredData.map((item) => (
-            <AlertItem key={item.id} item={item} />
-          ))}
+        <section className="flex flex-col gap-3 h-full w-full overflow-y-scroll">
+          {filteredData
+            .filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
+            .map((item) => (
+              <AlertItem key={item.id} item={item} />
+            ))}
         </section>
       </div>
-      <section className=" hidden md:flex flex-col items-center overflow-y-scroll  bg-[#181818] rounded-md w-[42%] h-full">
-        <Filters
-          filterOptions={filterOptions}
-          setFilterOptions={setFilterOptions}
-        />
+      <section className="hidden md:flex flex-col items-center overflow-y-scroll bg-[#181818] rounded-md w-[42%] h-full">
+        <Filters filterOptions={filterOptions} setFilterOptions={setFilterOptions} />
         <button
           onClick={applyFilters}
-          className="items-center mt-1 hover:bg-[#313131]  w-[40%] gap-2 rounded-lg  bg-[#53ACFF]  py-1.5 px-3 text-sm/6 font-semibold text-white  "
+          className="items-center mt-1 hover:bg-[#313131] w-[40%] gap-2 rounded-lg bg-[#53ACFF] py-1.5 px-3 text-sm/6 font-semibold text-white"
         >
           Apply
         </button>
@@ -156,7 +87,7 @@ const Alerts = () => {
           onClose={closeDialog}
         >
           <div className="fixed inset-0 z-10 w-screen overflow-y-auto backdrop-filter backdrop-blur-lg">
-            <div className="flex min-h-full items-center  justify-center p-4">
+            <div className="flex min-h-full items-center justify-center p-4">
               <TransitionChild
                 enter="ease-out duration-300"
                 enterFrom="opacity-0 transform-[scale(95%)]"
@@ -173,20 +104,17 @@ const Alerts = () => {
                   >
                     <Close className="h-4 w-4" />
                   </button>
-                  <Filters
-                    filterOptions={filterOptions}
-                    setFilterOptions={setFilterOptions}
-                  />
+                  <Filters filterOptions={filterOptions} setFilterOptions={setFilterOptions} />
                   <div className="w-full flex items-center justify-center gap-7">
                     <button
                       onClick={() => setIsOpen(false)}
-                      className="items-center mt-1 hover:bg-[#313131]  w-[40%] gap-2 rounded-lg border border-white  bg-transparent  py-1.5 px-3 text-sm/6 font-semibold text-white"
+                      className="items-center mt-1 hover:bg-[#313131] w-[40%] gap-2 rounded-lg border border-white bg-transparent py-1.5 px-3 text-sm/6 font-semibold text-white"
                     >
                       Save Filter
                     </button>
                     <button
                       onClick={applyFilters}
-                      className="items-center mt-1 hover:bg-[#313131]  w-[40%] gap-2 rounded-lg  bg-[#53ACFF]  py-1.5 px-3 text-sm/6 font-semibold text-white"
+                      className="items-center mt-1 hover:bg-[#313131] w-[40%] gap-2 rounded-lg bg-[#53ACFF] py-1.5 px-3 text-sm/6 font-semibold text-white"
                     >
                       Apply
                     </button>
